7caee5a3a1b03c7a02dc9cd7d423ea56729f3177
SimoneRicco <smnrcc0@gmail.com>
2023-06-26 18:01:42 +0200

    add cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,21 @@
     </head>
     <body>
         <h1 class="text-center">Ciao da vite</h1>
-        @dd($movies)
+        <div class="container d-flex flex-wrap">
+            @foreach ($movies as $item)
+            <div class="card m-auto" style="width: 18rem;">
+                <div class="card-header">
+                    <h5 class="card-title">{{ $item['title'] }}</h5>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Titolo originale: {{ $item['original_title'] }}</p>
+                    <p class="card-text">Nazionalità: {{ $item['nationality'] }}</p>
+                    <p class="card-text">Data: {{ $item['date'] }}</p>
+                    <p class="card-text">Voto: {{ $item['vote'] }}</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
         {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
     </body>
 </html>
